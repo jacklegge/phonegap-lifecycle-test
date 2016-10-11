b49482d69e86fff15210f0157871181830a7f930
Jack Legge <legj1_15@uni.worc.ac.uk>
2016-10-11 09:45:41 +0100

    update comments

--- www/js/index.js
+++ www/js/index.js
@@ -5,17 +5,20 @@ var launched_count = 0;
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
+        // setting up event listener for onDeviceReady function
     }
 	
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
+        // function above uses jQuery to increment counters and display text
 	}
 
 
     // device APIs are available
-    //
+    // deviceReady
+
     function onDeviceReady() {
 		alert("device ready");
         
@@ -34,6 +37,8 @@ var launched_count = 0;
 		updateDisplay();
     }
 	
+    // Handle the resume event
+    //
 	function onResume() {
 		alert("resume");
 		resumed_count++;
